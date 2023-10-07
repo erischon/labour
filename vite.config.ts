b3defe21e5843f8ff149f0a31d8c3862fb724b67
erischon <66017574+erischon@users.noreply.github.com>
2023-10-07 17:16:02 +0200

    setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./vitest.setup.ts"],
+    coverage: {
+      all: true,
+      exclude: [
+        "**/node_modules/**",
+        "**/vitest.config.ts",
+        "**/vitest.setup.ts",
+        "coverage/**",
+        "**/*.d.ts",
+        "**/postcss.config.js",
+        "**/tailwind.config.js",
+        "**/tsconfig.json",
+        "**/__tests__/**",
+      ],
+    },
+  },
+});
