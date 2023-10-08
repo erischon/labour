2e473db43dca3ffb802ee7ddd2578af019ec7cc8
erischon <66017574+erischon@users.noreply.github.com>
2023-10-08 17:50:00 +0200

    header

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,11 +1,23 @@
+import { HiPencilSquare } from "react-icons/hi2";
+
 export default function Header() {
   return (
-    <header>
-      <div>logo</div>
+    <header className="flex justify-between items-center px-10 py-5">
+      <div className="flex items-center gap-1">
+        <span className="text-2xl">
+          <HiPencilSquare />
+        </span>
 
-      <nav>navigation</nav>
+        <h2 className="text-2xl font-bold font-heading">Labour</h2>
+      </div>
 
-      <button>bouton</button>
+      <nav>
+        <ul>
+          <li>Home</li>
+        </ul>
+      </nav>
+
+      <button className="py-2 px-9 bg-slate-500">bouton</button>
     </header>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ["Poiret One", "cursive"],
+      },
+    },
   },
   plugins: [],
 };
