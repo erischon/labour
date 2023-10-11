65ff90c2e85868a05d129c09284101da4ce3eb7a
erischon <66017574+erischon@users.noreply.github.com>
2023-10-11 10:08:21 +0200

    Update Header.tsx

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,16 +1,37 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../../libs/firebase";
 import { HiPencilSquare } from "react-icons/hi2";
 import Modal from "../Modal";
+import FormAddTask from "../FormAddTask";
 
 export default function Header() {
   const [open, setOpen] = useState(false);
+  const [task, setTask] = useState("");
+
+  // Add a new task with a generated id.
+  async function addTask(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    try {
+      const docRef = await addDoc(collection(db, "tasks"), {
+        taskName: task,
+        createdAt: new Date(),
+        isDone: false,
+      });
+
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding task: ", e);
+    }
+  }
 
   return (
     <>
       <Modal open={open} onClose={() => setOpen(false)}>
-        Modal
+        <FormAddTask setTask={setTask} addTask={addTask} />
       </Modal>
 
       <header className="flex justify-between items-center px-10 py-5">
