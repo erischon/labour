6bea6b3f78d613d851eb90f93320ac9b00502661
erischon <66017574+erischon@users.noreply.github.com>
2023-10-13 11:10:21 +0200

    adding filter on tasks list

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 
 import { db } from "../libs/firebase";
@@ -13,6 +13,7 @@ import { getAllTasks } from "../libs/getAllTasks";
  */
 export default function Main() {
   const { tasks, setTasks, isModified, setIsModified } = useTaskContext();
+  const [filter, setFilter] = useState("all");
 
   // Delete a task.
   async function deleteTask(id: string) {
@@ -48,20 +49,68 @@ export default function Main() {
   return (
     <main className="my-10 px-10 py-5 max-w-4xl mx-auto">
       <div className="w-full">
-        <div className="grid grid-cols-12 px-5 py-2 mb-4 bg-slate-700 rounded-sm">
-          <div className="col-span-10 font-semibold">Tasks</div>
+        <div className="grid grid-cols-12 px-5 py-2 mb-3 bg-slate-700 rounded-sm">
+          <div className="col-span-10 flex items-center">
+            <p className="mr-10 font-semibold">Tasks</p>
+
+            <div className="flex items-end gap-6 text-sm h-full">
+              <label>
+                <input
+                  type="radio"
+                  name="filter"
+                  value="all"
+                  checked={filter === "all"}
+                  onChange={() => setFilter("all")}
+                />{" "}
+                All
+              </label>
+
+              <label>
+                <input
+                  type="radio"
+                  name="filter"
+                  value="done"
+                  checked={filter === "done"}
+                  onChange={() => setFilter("done")}
+                />{" "}
+                Done
+              </label>
+
+              <label>
+                <input
+                  type="radio"
+                  name="filter"
+                  value="undone"
+                  checked={filter === "undone"}
+                  onChange={() => setFilter("undone")}
+                />{" "}
+                Undone
+              </label>
+            </div>
+          </div>
+
           <div className="col-span-2 text-center font-semibold">Actions</div>
         </div>
 
         <div className="">
-          {tasks.map((task) => (
-            <TaskItem
-              key={task.id}
-              {...task}
-              toggleTask={toggleTask}
-              deleteTask={deleteTask}
-            />
-          ))}
+          {tasks
+            .filter((task) => {
+              if (filter === "all") {
+                return true;
+              } else if (filter === "done") {
+                return task.isDone;
+              } else {
+                return !task.isDone;
+              }
+            })
+            .map((task) => (
+              <TaskItem
+                key={task.id}
+                {...task}
+                toggleTask={toggleTask}
+                deleteTask={deleteTask}
+              />
+            ))}
         </div>
       </div>
     </main>
