0c98066cedfa0111d800aa0f87c1d17d295eb960
erischon <66017574+erischon@users.noreply.github.com>
2023-10-14 12:24:18 +0200

    add  page

--- src/routes/Tasks.route.tsx
+++ src/routes/Tasks.route.tsx
@@ -1,27 +1,41 @@
-import { useLoaderData } from "react-router-dom";
+import { collection, addDoc } from "firebase/firestore";
 
+import { db } from "../libs/firebase";
 import { TaskContextProvider } from "../contexts/TaskContext.tsx";
-import { ModalContextProvider } from "../contexts/ModalContext.tsx";
 
 import Header from "../components/layouts/Header";
 import Footer from "../components/layouts/Footer";
+import FormAddTask from "../components/FormAddTask";
 
 export default function TasksRoute() {
-  const tasks = useLoaderData();
+  // Add a new task with a generated id.
+  async function addTask(task: string) {
+    try {
+      const docRef = await addDoc(collection(db, "tasks"), {
+        taskName: task,
+        createdAt: new Date(),
+        isDone: false,
+      });
 
-  console.log(tasks);
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding task: ", e);
+    }
+  }
+
   return (
     <>
       <TaskContextProvider>
-        <ModalContextProvider>
-          <div className="w-full h-screen flex flex-col justify-between">
-            <Header />
+        <div className="w-full h-screen flex flex-col justify-between">
+          <Header />
 
-            <div className="flex flex-1">tasks route</div>
-
-            <Footer />
+          <div className="flex-1">
+            <h1>Add Task</h1>
+            <FormAddTask addTask={addTask} />
           </div>
-        </ModalContextProvider>
+
+          <Footer />
+        </div>
       </TaskContextProvider>
     </>
   );
