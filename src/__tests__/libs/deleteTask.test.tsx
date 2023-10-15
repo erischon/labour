ba5a1e7297ad2ef6ef536e18eaf20cdcff409193
erischon <66017574+erischon@users.noreply.github.com>
2023-10-15 17:11:07 +0200

    mock in progress

---
+++ src/__tests__/libs/deleteTask.test.tsx
@@ -0,0 +1,9 @@
+import { vi, it, expect, describe } from "vitest";
+
+import { deleteTask } from "../../libs/deleteTask";
+
+it("should delete task", () => {
+  // Arrange
+  // Act
+  // Assert
+});
