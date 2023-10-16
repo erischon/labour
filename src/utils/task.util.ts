a0af29d27c17fc4a57da3861b2c784d0a57e653b
erischon <66017574+erischon@users.noreply.github.com>
2023-10-16 13:56:58 +0200

    cleaning firestore

--- src/utils/task.util.ts
+++ src/utils/task.util.ts
@@ -1,7 +1,3 @@
-import { doc, updateDoc } from "firebase/firestore";
-
-import { db } from "../libs/firebase";
-
 type Task = {
   id: string;
   taskName: string;
@@ -43,22 +39,6 @@ export async function deleteTask(id: string) {
 }
 
 /**
- * Edits a task with the updated task name.
- * @param updatedTask - The updated task name.
- * @returns Promise<void>
- */
-export async function editTask(id: string, updatedTask: string): Promise<void> {
-  const tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
-  const task = tasks.find((task: Task) => task.id === id);
-
-  const taskIndex = tasks.findIndex((task: Task) => task.id === id);
-
-  tasks[taskIndex] = { ...task, taskName: updatedTask };
-
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-}
-
-/**
  * Retrieves all tasks from local storage.
  * @returns {Promise<Array>} An array of tasks.
  */
@@ -82,14 +62,33 @@ export async function getTask(id: string) {
 }
 
 /**
+ * Edits a task with the updated task name.
+ * @param updatedTask - The updated task name.
+ * @returns Promise<void>
+ */
+export async function editTask(id: string, updatedTask: string): Promise<void> {
+  const tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+  const task = tasks.find((task: Task) => task.id === id);
+
+  const taskIndex = tasks.findIndex((task: Task) => task.id === id);
+
+  tasks[taskIndex] = { ...task, taskName: updatedTask };
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+/**
  * Toggles the isDone property of a task in the "tasks" collection in Firestore.
  * @param id The ID of the task to toggle.
  * @param isDone The current value of the task's isDone property.
  */
 export async function toggleTask(id: string, isDone: boolean) {
-  const docRef = doc(db, "tasks", id);
+  const tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+  const task = tasks.find((task: Task) => task.id === id);
 
-  await updateDoc(docRef, {
-    isDone: !isDone,
-  });
+  const taskIndex = tasks.findIndex((task: Task) => task.id === id);
+
+  tasks[taskIndex] = { ...task, isDone: !isDone };
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
