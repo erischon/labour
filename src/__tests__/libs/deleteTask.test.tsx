57b29ba0ed248333f15b9d3f8796ab23fb1ce84b
erischon <66017574+erischon@users.noreply.github.com>
2023-10-16 10:11:16 +0200

    Delete deleteTask.test.tsx

--- src/__tests__/libs/deleteTask.test.tsx
+++
@@ -1,9 +0,0 @@
-import { vi, it, expect, describe } from "vitest";
-
-import { deleteTask } from "../../utils/deleteTask";
-
-it("should delete task", () => {
-  // Arrange
-  // Act
-  // Assert
-});
