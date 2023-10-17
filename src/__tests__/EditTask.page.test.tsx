543ca506b83146fc8bdfe917ff8cee970d516c41
erischon <66017574+erischon@users.noreply.github.com>
2023-10-17 16:11:35 +0200

    Update EditTask.page.test.tsx

--- src/__tests__/EditTask.page.test.tsx
+++ src/__tests__/EditTask.page.test.tsx
@@ -1,4 +1,4 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { screen } from "@testing-library/react";
 
 import { renderWithRouter } from "./utils";
@@ -6,6 +6,23 @@ import { renderWithRouter } from "./utils";
 import { TaskContextProvider } from "../contexts/TaskContext";
 
 import EditTaskPage from "../pages/EditTask.page";
+
+vi.mock("react-router-dom", async () => {
+  const actual: any = await vi.importActual("react-router-dom");
+
+  return {
+    ...actual,
+    useLoaderData: () => ({
+      task: {
+        id: "1",
+        taskName: "Task 1",
+        createdAt: new Date(),
+        isDone: false,
+      },
+      id: "1",
+    }),
+  };
+});
 
 describe("EditTaskPage", () => {
   it("should render the title Edit Task", () => {
