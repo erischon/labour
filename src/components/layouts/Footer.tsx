2f922cd76af9ce739298a706e1a80c1c444500c1
erischon <66017574+erischon@users.noreply.github.com>
2023-10-23 08:30:07 +0200

    header responsive

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -4,8 +4,19 @@
  */
 export default function Footer() {
   return (
-    <footer className="px-10 py-5 w-full max-w-5xl mx-auto text-xs">
-      <p>© 2023-{new Date().getFullYear()} Eri Schön All rights reserved</p>
+    <footer className="px-10 py-5 w-full max-w-5xl mx-auto text-xs text-center">
+      <p>
+        © 2023-{new Date().getFullYear()}{" "}
+        <a
+          href="https://www.erischon.dev"
+          target="_blank"
+          className="text-bold text-slate-300 hover:text-slate-50"
+        >
+          {" "}
+          Eri Schön
+        </a>{" "}
+        All rights reserved
+      </p>
     </footer>
   );
 }
--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { HiPencilSquare } from "react-icons/hi2";
 import { MdAddTask } from "react-icons/md";
 
+import MobileMenu from "../MobileMenu";
+
 /**
  * Renders the header component with a logo, navigation links and an "Add Task" button.
  * @returns JSX.Element
@@ -11,7 +13,7 @@ export default function Header(): JSX.Element {
   return (
     <>
       <header className="flex justify-between items-center w-full px-10 py-5 max-w-5xl mx-auto">
-        <Link to="/" className="flex items-center gap-1">
+        <Link to="/" className="flex items-center gap-1 mx-auto md:mx-0">
           <span className="text-2xl">
             <HiPencilSquare />
           </span>
@@ -21,15 +23,17 @@ export default function Header(): JSX.Element {
           </h2>
         </Link>
 
-        <nav>
-          <ul>
+        <nav className="">
+          <ul className="hidden md:block">
             <li>
               <Link to="/">Home</Link>
             </li>
           </ul>
         </nav>
 
-        <Link to="/tasks/add">
+        <MobileMenu />
+
+        <Link to="/tasks/add" className="hidden md:block">
           <button className="py-2 px-6 bg-slate-500 flex items-center gap-2">
             <div className="text-xl text-slate-100">
               <MdAddTask />
